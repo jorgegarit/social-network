d88066458872dbab57a555cd4fcb59fcbde158df
Jorge Garit <jorgegarit@gmail.com>
2022-10-28 11:57:16 -0400

    added thought text schema and createdAT but need to go to utilities and create the time stamp method

---
+++ models/Thought.js
@@ -0,0 +1,18 @@
+const { Schema, model, Types } = require('mongoose');
+
+//  schema for thoughts
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+    }
+)
