737d9279e35ade91a420d20cd8390717fc70bf22
Jorge Garit <jorgegarit@gmail.com>
2022-10-28 19:30:23 -0400

    controller for thoughts completed

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -32,5 +32,56 @@ const thoughtController = {
         });
     },
 
+    // create a new thought that will be paired with user id 
+    addThought({ params, body }, res) {
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+              { _id: params.userId },
+              { $push: { thoughts: _id } },
+              { new: true }
+            );
+          })
+          .then(dbThoughtData => {
+            if (!dbThoughtData) {
+              res.status(404).json({ message: 'No thought found under this id' });
+              return;
+            }
+            res.json(dbThoughtData);
+          })
+          .catch(error => res.json(error));
+    },
+
+    //   update a thought
+    updateThought({ params, body }, res) {
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true })
+        .then(dbPizzaData => {
+            if (!dbPizzaData) {
+            res.status(404).json({ message: 'No pizza found with this id!' });
+            return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(error => res.status(400).json(error));
+    },
+
+    // delete a thought
+    deleteThought({ params }, res) {
+        Thought.findOneAndDelete({ _id: params.thoughtid })
+        .then(erasedThought => {
+            if (!erasedThought) {
+            res.status(404).json({ message: 'Could not find a thought' });
+            }
+            return User.findOneAndUpdate(
+                { _id: params.thoughtId },
+                { $pull: { thoughts: params.thoughtId } },
+                { new: true }
+            );
+        })
+        .then(() => {
+            res.json({ message: 'The thought has been deleted' });
+        })
+        .catch(error => res.status(400).json(error));
+    }
 
 }
