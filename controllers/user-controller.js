19211b14028f131839a6d47efba6187009648dff
Jorge Garit <jorgegarit@gmail.com>
2022-10-28 12:45:09 -0400

    using the pizza data to help format the controller for User

---
+++ controllers/user-controller.js
@@ -0,0 +1,44 @@
+const { User, Thought } = require('../models');
+
+const userController = {
+    //  get all users
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(error => {
+            console.log(error);
+            res.status(400).json(error);
+        });
+    },
+
+    // get a specific user by their id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+        .then(dbUserData => {
+            if (!dbUserData) {
+            res.status(404).json({ message: 'No user found under this id' });
+            return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400).json(error);
+        });
+    },
+}
