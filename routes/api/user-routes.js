e87706c17f567251ce68b0a0052962c2cba6ec71
Jorge Garit <jorgegarit@gmail.com>
2022-10-28 13:13:01 -0400

    added the user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,35 @@
+//  will be getting the user routes from the user-controllers.js file
+const router = require('express').Router();
+
+// routes for user get and post  /api/users
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addNewFriend,
+    deleteFriend
+} = require('../../controllers/user-controller');
+
+// get/post routes
+router
+  .route('/')
+  .get(getAllUsers)
+  .post(createUser);
+
+// routes for user by id
+router
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+//  routes relating to friends
+router
+  .route('/:id/friends/:friendId')
+  .put(addNewFriend)
+  .delete(deleteFriend);
+
+// exports user routes
+module.exports = router;
