7e06359b33d0d6745985cc0e53e4d1eca81d2971
Jorge Garit <jorgegarit@gmail.com>
2022-10-28 10:21:57 -0400

    for user model set up username and email

---
+++ models/index.js
@@ -0,0 +1,19 @@
+//  user model using mongoose
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill out a valid email address!']
+        }
+    }
+)
